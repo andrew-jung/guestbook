1f79162b96a708c8bedb4869d2466ec5fb3ad106
Andrew Jung <andrewdhjung@gmail.com>
2020-02-19 17:29:40 -0500

    routes, templates

--- src/clj/guestbook/routes/home.clj
+++ src/clj/guestbook/routes/home.clj
@@ -5,10 +5,46 @@
    [clojure.java.io :as io]
    [guestbook.middleware :as middleware]
    [ring.util.response]
-   [ring.util.http-response :as response]))
+   [ring.util.http-response :as response]
+   [struct.core :as st]))
 
-(defn home-page [request]
-  (layout/render request "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+; Struct lib: http://funcool.github.io/struct/latest/ Validation library
+
+(def message-schema
+  "Define a message schema with required fields"
+  [[:name
+    st/required
+    st/string]
+
+   [:message
+    st/required
+    st/string
+    {:message "message must contain at least 10 characters"
+     :validate #(> (count %) 9)}]])
+
+(defn validate-message
+  "Validate the passed message"
+  [params]
+  (first (st/validate params message-schema)))
+
+(defn save-message!
+  "Try to save message, first validate, then add timestamp"
+  [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (response/found "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message!
+       (assoc params :timestamp (java.util.Date.)))
+      (response/found "/"))))
+
+; Built-in Luminus routers/handlers
+(defn home-page [{:keys [flash] :as request}]
+  (layout/render
+   request
+   "home.html"
+   (merge {:messages (db/get-messages)}
+          (select-keys flash [:name :message :errors]))))
 
 (defn about-page [request]
   (layout/render request "about.html"))
@@ -17,6 +53,6 @@
   [""
    {:middleware [middleware/wrap-csrf
                  middleware/wrap-formats]}
-   ["/" {:get home-page}]
+   ["/" {:get home-page
+         :post save-message!}]
    ["/about" {:get about-page}]])
-
